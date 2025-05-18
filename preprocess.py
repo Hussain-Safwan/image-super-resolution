cf43ae92ec6c888e2087652aaa391d8390e30aa8
Hussain Safwan <safwan.du16@gmail.com>
2025-05-18 20:32:04 +0600

    single image preprocessing

--- preprocess.py
+++ preprocess.py
@@ -2,8 +2,10 @@ import cv2
 import numpy as np
 from PIL import Image
 import os
+import sys
+from utils import get_config
 
-def create_narrow_fov(image, ratio=3/5):
+def create_narrow_fov(image, ratio):
     width, height = image.size
     new_width = int(width * ratio)  
     new_height = int(height * ratio)  
@@ -29,29 +31,48 @@ def downsize_wide_fov(image, factor=2):
 
 if __name__ == "__main__":
 
-    cnt = 1
-    limit = 500
-    home_dir = './dataset/lhq-kaggle/dataset'
-    original_dir = './dataset/images/original'
-    wide_dir = './dataset/images/wide'
-    narrow_dir = './dataset/images/narrow'
-    
-    original_images = os.listdir(home_dir)
+    narrow_ratio = get_config('narrow_ratio')
+    single = sys.argv[1]
 
-    for filename in original_images:
-        img = Image.open(f'{home_dir}/{filename}')
-        narrow_img = create_narrow_fov(img)
-        wide_img = downsize_wide_fov(img)
+    if single:
 
-        img.save(f'{original_dir}/{filename}')
-        narrow_img.save(f'{narrow_dir}/{filename}')
-        wide_img.save(f'{wide_dir}/{filename}')
+        img_path = sys.argv[2]
+        image = Image.open(img_path)
+        filename = os.path.basename(img_path).split('.')[0]
+        wide = downsize_wide_fov(image)
+        narrow = create_narrow_fov(image, narrow_ratio)
 
-        print(f'{cnt} - {filename}')
-        cnt = cnt+1
+        uploads_path = get_config('uploads_path')
+        narrow.save(f'{uploads_path}/{filename}_wide.jpg')
+        wide.save(f'{uploads_path}/{filename}_narrow.jpg')
 
-        if (cnt > limit):
-            break
+        print(f'Processed narrow and wide images save at {uploads_path}')
+
+    else:
+
+        cnt = 1
+        limit = 500
+        home_dir = './dataset/lhq-kaggle/dataset'
+        original_dir = './dataset/images/original'
+        wide_dir = './dataset/images/wide'
+        narrow_dir = './dataset/images/narrow'
+        
+        original_images = os.listdir(home_dir)
+
+        for filename in original_images:
+            img = Image.open(f'{home_dir}/{filename}')
+            narrow_img = create_narrow_fov(img)
+            wide_img = downsize_wide_fov(img)
+
+            img.save(f'{original_dir}/{filename}')
+            narrow_img.save(f'{narrow_dir}/{filename}')
+            wide_img.save(f'{wide_dir}/{filename}')
+
+            print(f'{cnt} - {filename}')
+            cnt = cnt+1
+
+            if (cnt > limit):
+                break
 
 
     
